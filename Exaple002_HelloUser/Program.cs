a95b943aee2b126a92129f7f016d05622b861dea
Татьяна <tattypaw@gmail.com>
2022-08-18 13:06:15 +0800

    HelloUser

---
+++ Exaple002_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+// See https://aka.ms/new-console-template for more information
+Console.Write("Введите Ваше имя: ");
+string username2 = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username2);
