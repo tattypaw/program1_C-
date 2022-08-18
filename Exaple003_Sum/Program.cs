d11a2cb23a37a06b7bae71a8f63d04b4bb64ee32
Татьяна <tattypaw@gmail.com>
2022-08-18 19:58:17 +0800

    random sum

--- Exaple003_Sum/Program.cs
+++ Exaple003_Sum/Program.cs
@@ -1,5 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿//int numberA = 3;
+//int numberB = 5;
 //Console.WriteLine(numberA + numberB);
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
  int result = numberA + numberB;
  Console.WriteLine(result);
